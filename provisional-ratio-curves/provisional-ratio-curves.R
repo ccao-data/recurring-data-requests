a67cd9f77cb1f2d1a42cccc95c068832c82f5de2
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2025-10-28 11:11:40 -0500

    Update provisional-ratio-curves/provisional-ratio-curves.R
    Co-authored-by: Jean Cochrane <jeancochrane@users.noreply.github.com>

--- provisional-ratio-curves/provisional-ratio-curves.R
+++ provisional-ratio-curves/provisional-ratio-curves.R
@@ -28,8 +28,8 @@ AWS_ATHENA_CONN_NOCTUA <- dbConnect(noctua::athena(), rstudio_conn_tab = FALSE)
 # from a local machine. They MUST be named according to the current naming
 # scheme, and there must be PIN and Desk Review Value columns
 data_path <- "O:/CCAODATA/recurring-data-requests/provisional-ratio-curves/"
-input_path <- file.path(data_path, "input_data")
-output_path <- file.path(data_path, "output_data")
+input_path <- file.path(data_path, "input")
+output_path <- file.path(data_path, "output")
 files_in <- list.files(input_path, full.names = TRUE)
 
 # Flatfile ----
