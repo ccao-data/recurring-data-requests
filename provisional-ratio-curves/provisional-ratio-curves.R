d500e979351b46b7a2d0a820fa70ebd78b09b4c8
William Ridgeway <william.ridgeway@cookcountyil.gov>
2026-04-20 12:12:01 -0500

    Add year folders to avoid no sale prices for old towns

--- provisional-ratio-curves/provisional-ratio-curves.R
+++ provisional-ratio-curves/provisional-ratio-curves.R
@@ -22,14 +22,16 @@ noctua_options(unload = TRUE)
 
 AWS_ATHENA_CONN_NOCTUA <- dbConnect(noctua::athena(), rstudio_conn_tab = FALSE)
 
+year <- format(Sys.Date(), "%Y")
+
 # These files live on the O drive in
 # O:/CCAODATA/recurring-data-requests/provisional-ratio-curves/. Since
 # that isn't accessible from the server they need to be copied locally or run
 # from a local machine. They MUST be named according to the current naming
 # scheme, and there must be PIN and Desk Review Value columns
 data_path <- "O:/CCAODATA/recurring-data-requests/provisional-ratio-curves/"
-input_path <- file.path(data_path, "input")
-output_path <- file.path(data_path, "output")
+input_path <- file.path(data_path, "input", year)
+output_path <- file.path(data_path, "output", year)
 files_in <- list.files(input_path, full.names = TRUE)
 
 # Flatfile ----
--- provisional-ratio-curves/provisional-ratio-curves.sql
+++ provisional-ratio-curves/provisional-ratio-curves.sql
@@ -8,7 +8,8 @@ WITH final_models AS (
         UNNEST(final_model.township_code_coverage) AS towns (township_code)
     -- Year will need to be adjusted so that desk review to model values join in
     -- R script is 1 to 1.
-    WHERE final_model.year = '2026' AND final_model.type = 'res'
+    WHERE final_model.year = CAST(YEAR(CURRENT_DATE) AS VARCHAR)
+        AND final_model.type = 'res'
 ),
 
 -- Use final model run IDs to grab the model values we need to compare Res Val's
@@ -40,7 +41,10 @@ most_recent_pin AS (
             ORDER BY uni.year DESC
         ) AS rank
     FROM default.vw_pin_universe AS uni
-    WHERE uni.year IN ('2025', '2026')
+    WHERE uni.year IN (
+            CAST(YEAR(CURRENT_DATE) - 1 AS VARCHAR),
+            CAST(YEAR(CURRENT_DATE) AS VARCHAR)
+        )
 )
 
 SELECT
