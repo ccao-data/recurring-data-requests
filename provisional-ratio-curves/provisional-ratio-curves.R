45ce22b9c1e439f9e432a74bd6bb9a94e03ec1f5
William Ridgeway <william.ridgeway@cookcountyil.gov>
2026-07-14 12:09:41 -0500

    Commenting

--- provisional-ratio-curves/provisional-ratio-curves.R
+++ provisional-ratio-curves/provisional-ratio-curves.R
@@ -74,7 +74,7 @@ all_ratios <- dr_vals %>%
       select(pin)
   ) %>%
   full_join(model_vals, by = "pin") %>%
-  # Define an excluded a sale as one that has a sale price but no provided desk
+  # Define an excluded sale as one that has a sale price but no provided desk
   # review value.
   mutate(sale_excluded = is.na(desk_review_value) & !is.na(sale_price))
 
