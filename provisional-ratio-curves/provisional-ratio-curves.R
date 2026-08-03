41bd86ba97eaa46e5c2352e6aaf8d910833083bf
Damonamajor <56321109+Damonamajor@users.noreply.github.com>
2026-08-03 15:14:10 -0500

    Update provisional-ratio-curves.R

--- provisional-ratio-curves/provisional-ratio-curves.R
+++ provisional-ratio-curves/provisional-ratio-curves.R
@@ -88,7 +88,8 @@ walk(unique(all_ratios$township_name), \(x) {
     select(-township_name) %>%
     mutate(
       price_decile = ntile(if_else(sale_excluded %in% TRUE,
-        NA_real_, sale_price), 10),
+        NA_real_, sale_price
+      ), 10),
       model_sale_ratio = model_value / sale_price,
       desk_review_sale_ratio = desk_review_value / sale_price
     )
